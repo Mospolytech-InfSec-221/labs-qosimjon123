bd28564ebb55639954f151e01d435d51f7988bbe
qosimjon123 <you@example.com>
2023-01-17 16:37:03 +0300

    exam

--- Source.cpp
+++ Source.cpp
@@ -10,6 +10,7 @@ private:
 	string line2;
 	fstream file;
 	fstream file2;
+	char bin[8];
 
 public:
 	picture_qosimov() 
@@ -19,7 +20,7 @@ public:
 	picture_qosimov(string filename) 
 	{
 		this->line = filename;
-		this->file.open(this->line, fstream::in | fstream::out | fstream::app|fstream::binary);
+		this->file.open(this->line, fstream::in | fstream::out | fstream::app | fstream::binary);
 	}
 	picture_qosimov(const picture_qosimov& other) {
 		this->line = other.line;
@@ -41,7 +42,22 @@ public:
 		}
 		
 	}
-	void read_pic( string line) 
+	/*char ascii_to_bin(int x) {
+		bin[8];
+		int j = 0;
+		int i;
+		for (i = 1; i < 9; i++) {
+			if ((x | (1 << 8 - i)) == x) {
+				bin[j] = '1';
+			}
+			else {
+				bin[j] = '0';
+			};
+			j++;
+		}
+		return bin;*/
+	//}
+	bool read_pic( string line) 
 	{
 		
 		if (read(line) )
@@ -49,13 +65,15 @@ public:
 			char ar;
 			file2.open("result.txt", fstream::in | fstream::out | fstream::app | fstream::binary);
 			while (file.get(ar)) {
-				cout << ar;
+				int a=ar;
+				cout << a;
 				file2 << ar;
 				line2 += ar;
 				ar =' ';
 
 			}
 			file2.close();
+			return true;
 		}
 	}
 	bool write(string filename)
@@ -86,6 +104,95 @@ public:
 		
 		
 	}
+	picture_qosimov& change_channel(char choice, double num) 
+	{
+
+	}
+	void change_color(char cvet, double num) {
+		char bufer;
+		int pixel;
+		for (int i = 0; i < 54; i++) {
+			bufer = file.get();
+
+		}
+		while (file.get(bufer)) {
+			std::cout << int(bufer) << " ";
+		}
+		cout << "vse";
+	};
+	int weight = 231;
+	int height = 231;
+	picture_qosimov& change_noir() {};
+	bool flop() {
+		
+		ifstream filecopy(this->line);
+		if (!filecopy.is_open()) {
+			cout << "cant open file " << endl;
+			return false;
+		}
+		string data;
+		string line;
+		while (getline(file, line))
+		{
+			data += line;
+		}
+		ofstream file_to("Source_copy.bmp");
+		if (file_to.is_open() != true) {
+			cout << "cant open copy file" << endl;
+			return false;
+		}
+		int k = size(data);
+		cout << k << endl;
+		for (int i = 0; i < height / 2; i++) {
+			for (int j = 0; j < weight; j++) {
+				char tmp = data[i * weight + j];
+				data[i * weight + j] = data[(231 - i) * weight + j];
+				data[i * weight + j] = tmp;
+			}
+		}
+
+		for (auto i : data) {
+			file_to << i;
+		}
+		cout << "horisontal reversed picture" << endl;
+		filecopy.close();
+		file_to.close();
+		return true;
+	}
+	bool flip() {
+
+		ifstream file(this->line);
+		if (!file.is_open()) {
+			cout << "cant open file" << endl;
+			return false;
+		}
+		string data;
+		string line;
+		while (getline(file, line))
+		{
+			data += line;
+		}
+		ofstream file_to("Source_copy2.bmp");
+		if (file_to.is_open() != true) {
+			cout << "cant open copy file" << endl;
+			return false;
+		}
+		for (int i = 0; i < height; i++) {
+			for (int j = 0; j < weight / 2; j++) {
+				char tmp = data[i + j * weight];
+				data[i + j * weight] = data[i + (231 - j) * weight];
+				data[i + j * weight] = tmp;
+			}
+		}
+		for (auto i : data) {
+			file_to << i;
+		}
+		cout << "kartina izmeneno" << endl;
+		file.close();
+		file_to.close();
+		return true;
+	}
+	picture_qosimov& rotate() {}
 	
 };
 int main() {
@@ -93,11 +200,11 @@ int main() {
 	string filename = "Source.bmp";
 	string filename2 = "Source2.bmp";
 	picture_qosimov picture(filename);
-	picture.read_pic(filename);
-	picture.write_pic(filename2);
+	///*picture.read_pic(filename);*/
+	picture.change_color(3, 34);
+	
 
-	/*file.open(filename, fstream::in | fstream::out | fstream::app|fstream::binary);
-	file.read()*/
+	
 
 	
 }
